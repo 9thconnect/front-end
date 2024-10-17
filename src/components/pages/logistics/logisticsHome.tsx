4070d8034cf02e578026895d00731e133d38dc7f
kodunmi <53086177+kodunmi@users.noreply.github.com>
2024-10-17 11:08:21 +0100

    added new images

--- src/components/pages/logistics/logisticsHome.tsx
+++ src/components/pages/logistics/logisticsHome.tsx
@@ -41,32 +41,102 @@ const LogisticPage = ({ slug }: { slug: string }) => {
 
   const companies = [
     {
-      name: "McDonald Trans",
-      location: "Abuja, NG",
-      phone: "+234 803 742 9208",
+      company: "Castel Majestic",
+      name: "Lexus 570",
+      phone: "090990948959",
       image:
-        "https://s3-alpha-sig.figma.com/img/84fa/22f7/34adbc36ff5d9021e9e47c2811332b6a?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jLAHB~RTntNHgspNnwUheRHQN2-Y-uOi6R2LFSFOL-EAR1kyXED-IyWaoHTBmm0x3z6PIoc1Ae-47NYfo~9Q5ffUfMCXCp7uDiQtOezgvq9XWapTAyONUsZXaOYEKorvyLqjfbzKhBH2vaR6xKZ7YMTgJoEr0E5JRWo2dMcpYifKHUBBQ157Y5rdhr2VQUdkRFkrWn~65cYKQk5gM3eUIbu1yK9HTrrOsZdfZKsCUQwXvnqTjRyfwG6WTmSpADIyPjNdsVn~t~ZicYNKykZuadI4gJYMSfgAPMeJ8ZoP4VHm-wybu1tuzbpfVcMFAAmNSrfxT93kZNpk710e5gglUw__",
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157766/2c72ccc451f07e40d9d6125358defb69_mrpd0c.png",
     },
     {
-      name: "7central Logistics",
-      location: "Abuja, NG",
-      phone: "+234 803 742 9208",
+      company: "Castel Majestic",
+      name: "Lexus 570 Bulletproof",
+      phone: "090990948959",
       image:
-        "https://res.cloudinary.com/dougwnqok/image/upload/v1729141898/b9dcffc9f4818ed9ee421597eab9f5bc_kd0b41.png",
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157772/e5f7e9506d03b9ffa1d46927d73f7be2_sbrj4y.png",
     },
     {
-      name: "Tayo Dispatch",
-      location: "Abuja, NG",
-      phone: "+234 803 742 9208",
+      company: "Castel Majestic",
+      name: "Toyota Prado",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157804/ceb5759790c26b108aca35d34750262c_c7mup6.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Lexus 300",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157811/d42105bf6a8bcfa2954b056ce65ef539_bsijtu.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Camry",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157858/daaa83447ec60bce73b6dce4bd45bffc_fz4tfs.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Highlander",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157876/2927a9aa2a5b3168c4bfd52a143cb284_qetmn1.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Venza",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157891/c31b7fd7402bfcd542eed8e1104e50fd_iwd4pi.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Honda CRV",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157899/3f7b4ba381c2df34c296b3baa9ca402c_bc4pnk.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Sienna",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157936/d8f00df0c7da22685c59ca09bac5eb0b_t1ltoc.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Tacoma",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729159300/425be6107684caa219b04c8e1a41edfd_jrkp6v.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Honda CRV",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157899/3f7b4ba381c2df34c296b3baa9ca402c_bc4pnk.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Highlander",
+      phone: "090990948959",
+      image:
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729157876/2927a9aa2a5b3168c4bfd52a143cb284_qetmn1.png",
+    },
+    {
+      company: "Castel Majestic",
+      name: "Qlink 300 CC",
+      phone: "090990948959",
       image:
         "https://res.cloudinary.com/dougwnqok/image/upload/v1729141921/753a3bc4117fdd251d9a7b89d8869765_nlh3og.jpg",
     },
     {
-      name: "7central Logistics",
-      location: "Abuja, NG",
-      phone: "+234 803 742 9208",
+      company: "Abuja, NG",
+      name: "Hiace Buses",
+      phone: "090990948959",
       image:
-        "https://res.cloudinary.com/dougwnqok/image/upload/v1729141937/c16610e14d7e7d2ee288c02d6baf6688_bzydy4.jpg",
+        "https://res.cloudinary.com/dougwnqok/image/upload/v1729159417/34adbc36ff5d9021e9e47c2811332b6a_tzrew0.jpg",
     },
   ];
 
@@ -131,7 +201,7 @@ const LogisticPage = ({ slug }: { slug: string }) => {
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {shuffleArray(companies).map((company, index) => (
+              {companies.slice(0, 4).map((company, index) => (
                 <div key={index} className="border rounded-lg overflow-hidden">
                   <div className="relative">
                     <img
@@ -144,7 +214,7 @@ const LogisticPage = ({ slug }: { slug: string }) => {
                     </div>
                   </div>
                   <div className="p-4">
-                    <p className="text-gray-500">{company.location}</p>
+                    <p className="text-gray-500">{company.company}</p>
                     <h3 className="font-semibold">{company.name}</h3>
                     <p className="text-gray-600">{company.phone}</p>
                   </div>
@@ -154,7 +224,7 @@ const LogisticPage = ({ slug }: { slug: string }) => {
 
             <div className="bg-[url('https://s3-alpha-sig.figma.com/img/d575/ad34/b2122640fc585f6ae4a664d8a171888a?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GoHqpwl12ApUwa~sJxIgV7562bsY6dzXkKiPgiFPxLh7P1cP9CEhEJDkinV4u0HjNbY5BuVwxSsFn1-rsYFn2PkhX-WQz~AktjVEpn9GixbVrcOIbtGvDzg3SKDHrdwJa9FbecrfJPt~yYTcwhnncBm27UZ5kFSCOs9-q1VTGHKSIhhBo4l8KJdAblzejITejc1aNnHoFUt~T4rLPzOhJCkxjrghXab9zBNHl5BA1KNG5ff4vIq~lqxtWfbEKJqpaQMW-ppJoMIQpFIYwm850Xz0c-IMR8ug8sFrjdoMYQQ4C3wCkNjfgcEPhG5it5mZLnBnirV9VuguMMinRrIZCg__')] bg-cover h-60 bg-no-repeat bg-center rounded-2xl mt-5 "></div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {shuffleArray(companies).map((company, index) => (
+              {companies.splice(4).map((company, index) => (
                 <div
                   key={index}
                   className="border rounded-lg overflow-hidden mt-6"
@@ -170,55 +240,7 @@ const LogisticPage = ({ slug }: { slug: string }) => {
                     </div>
                   </div>
                   <div className="p-4">
-                    <p className="text-gray-500">{company.location}</p>
-                    <h3 className="font-semibold">{company.name}</h3>
-                    <p className="text-gray-600">{company.phone}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {shuffleArray(companies).map((company, index) => (
-                <div
-                  key={index}
-                  className="border rounded-lg overflow-hidden mt-6"
-                >
-                  <div className="relative">
-                    <img
-                      src={company.image}
-                      alt={company.name}
-                      className="w-full h-48 object-cover"
-                    />
-                    <div className="absolute top-2 left-2 bg-blue-600 text-white px-2 py-1 rounded-full flex items-center">
-                      <Check size={16} className="mr-1" /> Verified
-                    </div>
-                  </div>
-                  <div className="p-4">
-                    <p className="text-gray-500">{company.location}</p>
-                    <h3 className="font-semibold">{company.name}</h3>
-                    <p className="text-gray-600">{company.phone}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {shuffleArray(companies).map((company, index) => (
-                <div
-                  key={index}
-                  className="border rounded-lg overflow-hidden mt-6"
-                >
-                  <div className="relative">
-                    <img
-                      src={company.image}
-                      alt={company.name}
-                      className="w-full h-48 object-cover"
-                    />
-                    <div className="absolute top-2 left-2 bg-blue-600 text-white px-2 py-1 rounded-full flex items-center">
-                      <Check size={16} className="mr-1" /> Verified
-                    </div>
-                  </div>
-                  <div className="p-4">
-                    <p className="text-gray-500">{company.location}</p>
+                    <p className="text-gray-500">{company.company}</p>
                     <h3 className="font-semibold">{company.name}</h3>
                     <p className="text-gray-600">{company.phone}</p>
                   </div>
