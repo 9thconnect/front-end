f5138481108fb3004c12bff5387c85149b6cca56
kodunmi <53086177+kodunmi@users.noreply.github.com>
2024-11-14 08:59:16 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@tanstack/query-core"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
