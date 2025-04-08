1c685840fae5634bd6788e948afbafdd9a083ad2
kodunmi <53086177+kodunmi@users.noreply.github.com>
2025-04-08 15:33:16 +0100

    updates

--- src/components/modals/notCustomer.tsx
+++ src/components/modals/notCustomer.tsx
@@ -29,52 +29,51 @@ const NotCustomerModal = () => {
   };
   const router = useRouter();
   return (
-    <div>
-      <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <AlertDialogContent className="max-w-xl max-h-screen overflow-y-auto text-offBlack">
-          <AlertDialogHeader className="flex flex-row items-center">
-            <AlertDialogCancel
-              className="bg-gray-100 rounded-full p-1 h-10 w-10 mr-3"
-              onClick={() => setIsDialogOpen(false)}
-            >
-              <X size={15} />
-            </AlertDialogCancel>
-            <AlertDialogTitle>Only Customers</AlertDialogTitle>
-          </AlertDialogHeader>
-          <div className=" flex flex-col items-center mt-8">
-            <p>
-              You need to be a customer to complete this action, please sign in
-              as a customer to continue
-            </p>
-            <img
-              className="h-2/3"
-              alt="access denied"
-              src={"/images/noaccess.png"}
-            />
-          </div>
+    <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <AlertDialogContent className="max-w-xl max-h-screen overflow-y-auto text-offBlack">
+        <AlertDialogHeader className="flex flex-row items-center">
+          <AlertDialogCancel
+            className="bg-gray-100 rounded-full p-1 h-10 w-10 mr-3"
+            onClick={() => setIsDialogOpen(false)}
+          >
+            <X size={15} />
+          </AlertDialogCancel>
+          <AlertDialogTitle>Only Customers</AlertDialogTitle>
+        </AlertDialogHeader>
+        <div className=" flex flex-col items-center mt-8">
+          <p>
+            You need to be a customer to complete this action, please sign in as
+            a customer to continue
+          </p>
+          <img
+            className=" md:h-2/3 max-h-60"
+            alt="access denied"
+            src={"/images/noaccess.png"}
+          />
+        </div>
 
-          <div className="flex justify-center mt-4">
-            <Button
-              onClick={() => {
-                setIsDialogOpen(false);
-                router.push("/customer/register");
-              }}
-              className="mr-2"
-            >
-              Register as a Customer
-            </Button>
-            <Button
-              onClick={() => {
-                setIsDialogOpen(false);
-                router.push("/customer/login");
-              }}
-            >
-              Sign in as Customer
-            </Button>
-          </div>
-        </AlertDialogContent>
-      </AlertDialog>
-    </div>
+        <div className="md:flex justify-center mt-4">
+          <Button
+            onClick={() => {
+              setIsDialogOpen(false);
+              router.push("/customer/register");
+            }}
+            className="md:mr-2 mb-2 md:mb-0 w-full md:w-fit"
+          >
+            Register as a Customer
+          </Button>
+          <Button
+            onClick={() => {
+              setIsDialogOpen(false);
+              router.push("/customer/login");
+            }}
+            className="w-full md:w-fit"
+          >
+            Sign in as Customer
+          </Button>
+        </div>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
